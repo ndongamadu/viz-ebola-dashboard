f01cfb96e32ae71d7136d032794bf1aecf12013c
amadu <ndongamadou@gmail.com>
2020-01-29 13:32:20 +0000

    ejs

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // window.$ = window.jQuery = require('jquery');
 var express = require('express');
+var path = require('path');
+var routes = require('./routes')
+
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('<html><body><h1>Hello World</h1></body></html>');
-});
 
-var server = app.listen(5000, function () {
-    console.log('Node server is running..');
+app.set("port", process.env.PORT || 3000);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(routes);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(app.get("port"), function(){
+	console.log('Node server is running..');
 });
