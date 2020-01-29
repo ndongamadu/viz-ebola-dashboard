d2b14a6ede4e1c42e0150ad0eee1cf6c232d750a
amadu <ndongamadou@gmail.com>
2020-01-29 11:24:01 +0000

    express and test

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-window.$ = window.jQuery = require('jquery');
+// window.$ = window.jQuery = require('jquery');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
